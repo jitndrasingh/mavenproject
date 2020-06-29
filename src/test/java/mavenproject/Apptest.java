53d89dd0e3c5626621c2080931d02a577bb65c56
jitndrasingh <j.s.r.p.vingarla@gmail.com>
2020-06-29 13:01:21 +0530

    maven project using eclipse

---
+++ src/test/java/mavenproject/Apptest.java
@@ -0,0 +1,14 @@
+package mavenproject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Apptest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
